af79687cae9a7a3972bd64435065d5820e734a3e
Alejandro Alvarez <a.alejandro.alvarez.a@gmail.com>
2025-08-31 02:03:14 -0400

    feat(Header, ScaffoldEthAppWithProviders): update header and main layout styles
    - Changed header background color to improve contrast and aesthetics.
    - Updated dropdown menu border color for better visibility.
    - Enhanced main layout background color for consistency across the application.
    - Added background color to the body for a cohesive design.

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -81,7 +81,7 @@ export const Header = () => {
   );
 
   return (
-    <header className="sticky lg:static top-0 navbar bg-base-900 min-h-0 flex-shrink-0 justify-between z-20 px-0 sm:px-2 border-b border-[#252442]">
+    <header className="sticky lg:static top-0 navbar bg-base-100 min-h-0 flex-shrink-0 justify-between z-20 px-0 sm:px-2 border-b border-base-300">
       <div className="navbar-start w-auto lg:w-1/2">
         <div className="lg:hidden dropdown" ref={burgerMenuRef}>
           <label
@@ -97,7 +97,7 @@ export const Header = () => {
           {isDrawerOpen && (
             <ul
               tabIndex={0}
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+              className="menu border border-base-300 menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
               onClick={() => {
                 setIsDrawerOpen(false);
               }}
--- packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
+++ packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
@@ -26,9 +26,9 @@ const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <>
-      <div className="flex flex-col min-h-screen">
+      <div className="flex flex-col min-h-screen bg-base-100">
         <Header />
-        <main className="relative flex flex-col flex-1">{children}</main>
+        <main className="relative flex flex-col flex-1 bg-base-100">{children}</main>
         <Footer />
       </div>
       <Toaster />
